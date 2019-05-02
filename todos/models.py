15d6fcff2ed9110592e5cc5814aaad6a6c712c89
choidoyeong <omok5674@naver.com>
2019-05-02 18:34:15 +0900

    modify models and make serializers

--- todos/models.py
+++ todos/models.py
@@ -8,8 +8,7 @@ class Tag(models.Model):
 
 class Todo(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASECADE)
-    todo_title = models.CharField(max_length=200)
-    todo_content = models.TextField()
+    content = models.CharField(max_length=200)
     tags = models.ManyToManyField(Tag, through='Categorize')
     success = models.BooleanField(defualt=False) 
     deadline = models.DateTimeField(null=True)
@@ -18,6 +17,6 @@ class Categorize(models.Model):
     tag  = models.ForeignKey(Tag, on_delete=models.CASECADE)
     todo = models.ForeignKey(Todo, on_delete=models.CASECADE)
 
-class Done(models.Model):
+class DoneTodo(models.Model):
     todo = models.ForeignKey(Todo, on_delete=models.CASECADE)
     done_date = models.DateField(auto_now_add = True)
--- todos/serializers.py
+++ todos/serializers.py
@@ -1,2 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Todo
+        fields = ('id','owner','content', 'tags', 'success','deadline')
+
+class DoneTodoSerailizer(serializers.ModelSerializer):
+    class Meta:
+        models = DoneTodo
+        fields = ('id', 'todo', 'done_date')
